69ecce8fa4661c63eabec3ad82e1c48f733feaa6
victoelim <vlxa@outlook.com>
2021-01-06 18:06:30 +0800

    fine tuned placeholder content

--- src/Article/article.js
+++ src/Article/article.js
@@ -6,7 +6,7 @@ const Article = () => {
         <>
             <div className = "article-div">
                 <h1 className = "article-title">ARTICLE TITLE</h1>
-                <img src = "https://via.placeholder.com/1080x720"/>
+                <img src = "https://via.placeholder.com/1440x612"/>
                 <div className = "article-text">
                     <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
